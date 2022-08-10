0a692f8efd8194312ffe54d574e776bfd7aa2a4b
Josephat <josephatn10@gmail.com>
2022-08-10 16:24:47 +0300

    start home app and create a custom nav

---
+++ Makefile
@@ -0,0 +1,15 @@
+serve:
+	python manage.py runserver
+
+migration: # suffix with the name of app to make migrations for
+	python manage.py makemigrations 
+
+migrate: # Apply migrations
+	python manage.py migrate 
+
+test: # suffix with the app name tby which to run tests for
+	python manage.py test 
+
+user: # create superuser
+	python manage.py createsuperuser
+	
---
+++ static/js/scripts.js
@@ -0,0 +1,18 @@
+(function ($) {
+	"use strict";
+		// navbar hover function
+	$("nav .dropdown").hover(
+		function () {
+			var $this = $(this);
+			$this.addClass("show");
+			$this.find("> a").attr("aria-expanded", true);
+			$this.find(".dropdown-menu").addClass("show");
+		},
+		function () {
+			var $this = $(this);
+			$this.removeClass("show");
+			$this.find("> a").attr("aria-expanded", false);
+			$this.find(".dropdown-menu").removeClass("show");
+		},
+	);
+})($);
