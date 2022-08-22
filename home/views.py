add0dc9caa4e42bf3620ad2f6537800c62bbea25
Josephat <josephatn10@gmail.com>
2022-08-22 16:25:44 +0300

    add gallery file and path url

--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html',{})
+    return render(request, 'home/home.html',{})
+
+def gallery(request):
+    return render(request, 'home/gallery.html',{})
