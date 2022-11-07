f16e6ac3bccf194f94ec8ad205b2366641c742bb
Josephat Ngugi <Josephatn10@gmail.com>
2022-11-07 23:26:07 +0300

    add query function for about pages

--- home/views.py
+++ home/views.py
@@ -1,18 +1,22 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import *
 
 # Create your views here.
 
 def home(request):
-    welcome_post = Post.objects.get(target_page__exact='WC')
-    about_post = Post.objects.get(target_page__exact='1C')
-    admission_post = Post.objects.get(target_page__exact='2C')
-    academics_post = Post.objects.get(target_page__exact='3C')
-    staff_post = Post.objects.get(target_page__exact='4C')
-    sports_post = Post.objects.get(target_page__exact='5C')
-    library_post = Post.objects.get(target_page__exact='6C')
-    alumni_post = Post.objects.get(target_page__exact='7C')
-    board_m_post = Post.objects.get(target_page__exact='8C')
+    try:
+        welcome_post = Post.objects.get(target_page__exact='WC')
+        about_post = Post.objects.get(target_page__exact='1C')
+        admission_post = Post.objects.get(target_page__exact='2C')
+        academics_post = Post.objects.get(target_page__exact='3C')
+        staff_post = Post.objects.get(target_page__exact='4C')
+        sports_post = Post.objects.get(target_page__exact='5C')
+        library_post = Post.objects.get(target_page__exact='6C')
+        alumni_post = Post.objects.get(target_page__exact='7C')
+        board_m_post = Post.objects.get(target_page__exact='8C')
+    except Post.DoesNotExist:
+        raise Http404("No Post is Currently available.")
 
     news = News.objects.all()[:4]
 
@@ -31,10 +35,23 @@ def home(request):
     return render(request, 'home/home.html', context)
 
 def gallery(request):
-    return render(request, 'home/gallery.html',{})
+    images = Gallery.objects.all().filter(category__gt=0).order_by('category')
+
+    context = {
+        'images': images,
+    }
+    return render(request, 'home/gallery.html', context)
 
 def about(request):
-    return render(request, 'home/about.html',{})
+    departments_posts = Post.objects.all().filter(department_id__gt=0)
+    school_info = Post.objects.filter(school_info_id__gt=0)
+    about = Post.objects.get(target_page__exact='1C')
+    context = {
+        'departments_posts': departments_posts,
+        'school_info': school_info,
+        'about': about,
+    }
+    return render(request, 'home/about.html', context)
 
 def contact(request):
     return render(request, 'home/contact.html',{})
