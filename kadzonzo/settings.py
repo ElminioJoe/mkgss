74196f7266d94e90790dd00e7008650d4b5a07e5
JosephatNgugi <Josephatn10@gmail.com>
2023-07-27 19:45:20 +0300

    modify database options to be used

--- kadzonzo/settings.py
+++ kadzonzo/settings.py
@@ -86,17 +86,25 @@ WSGI_APPLICATION = 'kadzonzo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': "db",  # set in docker-compose.yml
-        'PORT': os.environ.get('DB_PORT')
+# Check if the environment variable is set to use PostgreSQL or default to SQLite
+if "USE_POSTGRES" in os.environ and os.environ["USE_POSTGRES"] == "True":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': "db",  # set in docker-compose.yml
+            'PORT': os.environ.get('DB_PORT')
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
