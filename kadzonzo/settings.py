bc4a84dc9327a8ba8c2aafc9305f5dc6abd9c842
JosephatNgugi <josephatn10@gmail.com>
2024-03-15 14:34:34 +0300

    remove the check for debug status fo staticfiles rendering

--- kadzonzo/settings.py
+++ kadzonzo/settings.py
@@ -133,9 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
