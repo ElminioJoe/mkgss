218b702cef549aba4f28518048b185176f47f300
JosephatNgugi <josephatn10@gmail.com>
2024-03-15 16:19:43 +0300

    update production deployment settings

--- kadzonzo/settings.py
+++ kadzonzo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['*.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'moikadzonzogirls.sc.ke', 'mkgss.onrender.com']
 
 
 # Application definition
