d6c7a70a90984770327f1a4496455c360c331592
ElminioJoe <163521454+ElminioJoe@users.noreply.github.com>
2024-03-15 20:56:59 +0300

    Fix URLPatterns is not iterable

--- kadzonzo/urls.py
+++ kadzonzo/urls.py
@@ -33,4 +33,4 @@ from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 else:
-    urlpatterns+= re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT})
+    urlpatterns+= re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
