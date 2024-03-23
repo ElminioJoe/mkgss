ed8ca26d94d177ceb1dadd72419d43ea9ce18d4b
JosephatNgugi <josephatn10@gmail.com>
2024-03-23 11:49:44 +0300

    change debug

--- kadzonzo/settings.py
+++ kadzonzo/settings.py
@@ -26,7 +26,7 @@ load_dotenv(find_dotenv())
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS_ENV = os.environ.get("ALLOWED_HOSTS")
